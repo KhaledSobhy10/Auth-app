b1315dd96a4bf79c306faae5d0511b90162fc045
khaled-sobhy <khaledloda696@gmail.com>
2023-05-15 22:44:41 +0300

    Setup routes login and sign up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
+import {  Route, Router, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/Login'
 import SignUp from './pages/Signup'
 
 function App() {
 
-  return (
- <SignUp/>
+  return (   
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<SignUp/>} />
+    </Routes>
+
   )
 }
 
