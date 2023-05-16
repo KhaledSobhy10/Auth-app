ec5bc64843bf6c2fc1b7ac7fe3d6a4ce151da3d4
KhaledSobhy22 <Khaled.sobhy@easwaaqmisr.com>
2023-05-16 14:25:02 +0300

    Setup header for cros

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,26 @@ import bodyParser from "body-parser";
 import profileRouter from "./src/routes/profile";
 const app = express();
 
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(bodyParser.json());
 
 app.use("/api/profile", profileRouter);
