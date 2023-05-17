85c6a61768300dbb064c65354a8c688d11ef5027
KhaledSobhy22 <Khaled.sobhy@easwaaqmisr.com>
2023-05-17 15:17:30 +0300

    Add verify token

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { login } from "../controllers/auth";
+import {
+  postLogin,
+  postVerifyToken,
+  validateLoginInput,
+} from "../controllers/auth";
 
 const router = Router();
 
-router.post("/login", login);
+router.post("/login", [validateLoginInput], postLogin);
+
+router.post("/verify-token", postVerifyToken);
 
 export default router;
