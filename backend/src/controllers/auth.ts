35dcec2090647f6512977a4625c323715d3a7b8b
KhaledSobhy22 <Khaled.sobhy@easwaaqmisr.com>
2023-05-17 15:17:11 +0300

    Add validate login input middleware . Add verify token.

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,22 +1,14 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 import prismaDB from "../util/db";
 import { getKeysWithError } from "../util/zodErrorFormater";
-import { generateToken } from "../util/token";
+import { generateToken, verifyToken } from "../util/token";
 import { ProfileCreateInputSchema } from "../../prisma/generated/zod";
 
-export const login = async (req: Request, res: Response) => {
-  const data = { ...req.body };
-  // Check validation of inputs before check in DB
-  const validate = ProfileCreateInputSchema.safeParse(data);
-  if (!validate.success)
-    return res.status(400).json({
-      success: false,
-      errors: getKeysWithError(validate.error.issues),
-    });
+export const postLogin = async (req: Request, res: Response) => {
   // Check if data is exist in DB
   try {
     const user = await prismaDB.profile.findFirst({
-      where: { ...validate.data },
+      where: { ...req.body },
     });
     if (user) {
       const token = await generateToken({ id: user.id });
@@ -35,4 +27,37 @@ export const login = async (req: Request, res: Response) => {
       message: `Server Error ${error}`,
     });
   }
+};
+
+export const validateLoginInput = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const data = { ...req.body };
+  // Check validation of inputs before check in DB
+  const validate = ProfileCreateInputSchema.safeParse(data);
+  if (!validate.success)
+    return res.status(400).json({
+      success: false,
+      errors: getKeysWithError(validate.error.issues),
+    });
+  req.body = validate.data;
+  next();
+};
+
+export const postVerifyToken = async (req: Request, res: Response) => {
+  const { token } = req.body;
+  if (!token) {
+    res.status(400).json({ success: false, message: "Token is required !" });
+    return;
+  }
+  verifyToken(token)
+    .then((result) => {
+      console.log("result", result);
+      res.status(200).json({ success: true, message: "Valid token !" });
+    })
+    .catch((error) => {
+      res.status(400).json({ success: false, message: error.message });
+    });
 };
