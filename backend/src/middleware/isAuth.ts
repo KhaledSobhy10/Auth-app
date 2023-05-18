b020c7a809744d85f2d88714f8299c800c93b42a
KhaledSobhy22 <Khaled.sobhy@easwaaqmisr.com>
2023-05-18 15:51:53 +0300

    Add auth middleware

---
+++ backend/src/middleware/isAuth.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyToken } from "../util/token";
+import { JwtPayload } from "jsonwebtoken";
+
+export const isAuth = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader)
+    return res.json({
+      success: false,
+      message: "Authorization header is required !",
+    });
+  const token: string = authHeader.split(" ")[1];
+
+  verifyToken(token)
+    .then((result) => {
+      const { id } = result as JwtPayload;
+      req.params.id = id;
+      next();
+    })
+    .catch((error) => {
+      return res.status(401).json({ success: false, message: error.message });
+    });
+};
--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -1,10 +1,11 @@
-import { Router } from "express";
+import { Router, Response, Request, NextFunction } from "express";
 import { getProfile, getProfiles, postProfile } from "../controllers/profile";
+import { isAuth } from "../middleware/isAuth";
 
 const router = Router();
 
 router.post("/", postProfile);
-router.get("/", getProfiles);
-router.get("/:id", getProfile);
+// router.get("/", getProfiles);
+router.get("/", [isAuth], getProfile);
 
 export default router;
