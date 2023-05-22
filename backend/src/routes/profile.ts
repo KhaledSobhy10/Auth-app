9904fa0cbd5deb95ad9f9bf7e962c33daed584f0
khaled-sobhy <khaledloda696@gmail.com>
2023-05-23 01:48:51 +0300

    Add multer and validate profile middleware

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -1,11 +1,35 @@
-import { Router, Response, Request, NextFunction } from "express";
-import { getProfile, getProfiles, postProfile } from "../controllers/profile";
+import { Router } from "express";
+import {
+  getProfile,
+  getProfiles,
+  postProfile,
+  putProfile,
+} from "../controllers/profile";
 import { isAuth } from "../middleware/isAuth";
+import { isValidProfileInputs } from "../middleware/isValidProfile";
+import multer from "multer";
 
 const router = Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const ex = file.mimetype.split("/")[1];
+    const uniqueSuffix = Date.now() + "." + ex;
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+const upload = multer({ storage });
 
-router.post("/", postProfile);
+// routes
 // router.get("/", getProfiles);
 router.get("/", [isAuth], getProfile);
+router.post("/", [isValidProfileInputs], postProfile);
+router.put(
+  "/",
+  [isAuth, upload.single("photo"), isValidProfileInputs],
+  putProfile
+);
 
 export default router;
