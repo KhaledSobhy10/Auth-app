229550d2f495add171a3552c5c5ee27ba9c1d251
khaled-sobhy <khaledloda696@gmail.com>
2023-05-25 19:02:08 +0300

    Fix lint problem

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,26 +1,22 @@
-import * as React from 'react';
+import * as React from "react";
 import placeholder from "../assets/react.svg";
 import logo from "../assets/icons/logo.svg";
 
 import DropDown from "./DropDown";
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IHeaderProps {
-    userName: string
+  userName: string;
 }
 
 const Header: React.FunctionComponent<IHeaderProps> = (props) => {
-    return <header className='container flex justify-between py-6 px-6 '>
-        <img src={logo} />
-        <div className='flex items-center gap-3'>
-            <img src={placeholder} className='w-5 rounded-lg' />
-            <DropDown userName={props.userName} />
-        </div>
-    </header>;
+  return (
+    <header className="container flex justify-between px-6 py-6 ">
+      <img src={logo} />
+      <div className="flex items-center gap-3">
+        <img src={placeholder} className="w-5 rounded-lg" />
+        <DropDown userName={props.userName} />
+      </div>
+    </header>
+  );
 };
-
-
-
-
-
 
 export default Header;
--- frontend/src/pages/Auth/index.tsx
+++ frontend/src/pages/Auth/index.tsx
@@ -13,12 +13,10 @@ import { toast } from "react-hot-toast";
 import { USER_TOKEN } from "../../constants/keys";
 import { passwordRegex } from "../../util/common-regex";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IAuthProps {
-  isSignUp: boolean
-  reCheck: () => void
+  isSignUp: boolean;
+  reCheck: () => void;
 }
-
 
 const schema = z.object({
   email: z.string().email("Invalid Email !"),
@@ -32,39 +30,36 @@ const Auth: React.FunctionComponent<IAuthProps> = (props: IAuthProps) => {
     register,
     handleSubmit,
     formState: { errors },
-    setError
+    setError,
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
-  const URL = props.isSignUp ? "/profile" : "/auth/login"
-  const notifyError = (message: string) => toast.error(message, { id: "error-toast" });
+  const URL = props.isSignUp ? "/profile" : "/auth/login";
+  const notifyError = (message: string) =>
+    toast.error(message, { id: "error-toast" });
   const navigate = useNavigate();
   const mutation = useMutation({
     mutationFn: (data: FormData) => {
-      return axiosInstance.post(URL,
-        data
-      );
-    }
-    , onError(error, variables, context) {
-
+      return axiosInstance.post(URL, data);
+    },
+    onError(error, variables, context) {
       if (axios.isAxiosError(error)) {
         if (error.response?.data.errors) {
-          const errors = error.response?.data.errors as { key: "email" | "password", message: string }[];
-          errors.forEach(error => {
-            setError(error.key, { type: 'custom', message: error.message });
+          const errors = error.response?.data.errors as {
+            key: "email" | "password";
+            message: string;
+          }[];
+          errors.forEach((error) => {
+            setError(error.key, { type: "custom", message: error.message });
           });
-        } else
-          notifyError(error.response?.data.message || "Error");
-      }
-      else
-        notifyError("Error !!");
-
+        } else notifyError(error.response?.data.message || "Error");
+      } else notifyError("Error !!");
     },
     onSuccess(response, variables, context) {
       if (response.data && response.data.success) {
         if (response.data.data.token) {
-          localStorage.setItem(USER_TOKEN, response.data.data.token)
-          props.reCheck()
+          localStorage.setItem(USER_TOKEN, response.data.data.token);
+          props.reCheck();
         }
       }
     },
@@ -72,77 +67,103 @@ const Auth: React.FunctionComponent<IAuthProps> = (props: IAuthProps) => {
 
   const registerProfile = (data: FormData) => {
     mutation.mutate(data);
-  }
-
-  const onsubmit: SubmitHandler<FormData> = (data) => {
-    registerProfile(data)
   };
 
+  const onsubmit: SubmitHandler<FormData> = (data) => {
+    registerProfile(data);
+  };
 
   return (
-    <main className="min-h-screen w-screen bg-white flex justify-center items-center">
-      <div className="border rounded-3xl sm:px-12 sm:py-10 px-6 py-5 m-1 border-borderColor sm:w-[450px] w-11/12">
+    <main className="flex min-h-screen w-screen items-center justify-center bg-white">
+      <div className="m-1 w-11/12 rounded-3xl border border-borderColor px-6 py-5 sm:w-[450px] sm:px-12 sm:py-10">
         <div>
           <img src={logo} />
         </div>
-        <h1 className="font-semibold text-primaryText text-lg mt-8">
-          {props.isSignUp ? "Join thousands of learners from around the world" : "Login"}
+        <h1 className="mt-8 text-lg font-semibold text-primaryText">
+          {props.isSignUp
+            ? "Join thousands of learners from around the world"
+            : "Login"}
         </h1>
-        {props.isSignUp && <h2 className="mt-3 font text-primaryText">
-          Master web development by making real-life projects. There are
-          multiple paths for you to choose
-        </h2>}
+        {props.isSignUp && (
+          <h2 className="font mt-3 text-primaryText">
+            Master web development by making real-life projects. There are
+            multiple paths for you to choose
+          </h2>
+        )}
 
         <form
           className="mt-8 flex flex-col gap-7"
           onSubmit={handleSubmit(onsubmit)}
           noValidate
         >
-          <div className={` border ${errors?.email ? "border-pink-600 " : "border-borderColor "} rounded-xl flex p-3 gap-3 relative `}>
-            <div className="text-pink-700 text-sm absolute top-full right-0">{errors?.email?.message}</div>
+          <div
+            className={` border ${
+              errors?.email ? "border-pink-600 " : "border-borderColor "
+            } relative flex gap-3 rounded-xl p-3 `}
+          >
+            <div className="absolute right-0 top-full text-sm text-pink-700">
+              {errors?.email?.message}
+            </div>
             <img src={emailIcon} className="w-5 " />
             <input
-              className={` outline-none flex-1 text-primaryText placeholder-secondaryText `}
+              className={` flex-1 text-primaryText placeholder-secondaryText outline-none `}
               type="email"
               placeholder="Email"
               {...register("email")}
             />
           </div>
 
-
-          <div className={` border ${errors?.password ? "border-pink-600 " : "border-borderColor "} rounded-xl flex p-3 gap-3 relative `}>
-            <div className="text-pink-700 text-sm absolute top-full right-0">{errors?.password?.message}</div>
+          <div
+            className={` border ${
+              errors?.password ? "border-pink-600 " : "border-borderColor "
+            } relative flex gap-3 rounded-xl p-3 `}
+          >
+            <div className="absolute right-0 top-full text-sm text-pink-700">
+              {errors?.password?.message}
+            </div>
 
             <img src={lockIcon} className="w-5 " />
             <input
-              className="outline-none flex-1 text-primaryText placeholder-secondaryText"
+              className="flex-1 text-primaryText placeholder-secondaryText outline-none"
               type="text"
               placeholder="Password"
               {...register("password")}
             />
           </div>
-          <button className="flex items-center justify-center gap-4 bg-accent rounded-xl text-white py-2 disabled:opacity-50" disabled={mutation.isLoading}>
+          <button
+            className="flex items-center justify-center gap-4 rounded-xl bg-accent py-2 text-white disabled:opacity-50"
+            disabled={mutation.isLoading}
+          >
             {props.isSignUp ? "Start coding now" : "Login"}
-            {mutation.isLoading &&
-              <div className=" animate-spin inline-block w-4 h-4 border-[3px] border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading">
+            {mutation.isLoading && (
+              <div
+                className=" inline-block h-4 w-4 animate-spin rounded-full border-[3px] border-current border-t-transparent text-white"
+                role="status"
+                aria-label="loading"
+              >
                 <span className="sr-only">Loading...</span>
-              </div>}
+              </div>
+            )}
           </button>
         </form>
-        {props.isSignUp ? <div className="mt-8 mx-auto text-sm text-center text-secondaryText">
-          Adready a member?
-          <Link to={"/login"} className="text-accent">
-            Login
-          </Link>
-        </div> : <div className="mt-8 mx-auto text-sm text-center text-secondaryText">Don’t have an account yet? <Link to={"/register"} className="text-accent">Register</Link></div>
-        }
-
-
+        {props.isSignUp ? (
+          <div className="mx-auto mt-8 text-center text-sm text-secondaryText">
+            Adready a member?
+            <Link to={"/login"} className="text-accent">
+              Login
+            </Link>
+          </div>
+        ) : (
+          <div className="mx-auto mt-8 text-center text-sm text-secondaryText">
+            Don’t have an account yet?{" "}
+            <Link to={"/register"} className="text-accent">
+              Register
+            </Link>
+          </div>
+        )}
       </div>
     </main>
   );
 };
 
 export default Auth;
-
-
