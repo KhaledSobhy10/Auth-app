4739c50c9d47eb6b5d371963eacd388a76d01fba
khaled-sobhy <khaledloda696@gmail.com>
2023-05-27 20:28:51 +0300

    Handling error

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -2,15 +2,43 @@ import { useEffect, useState } from "react";
 import { USER_TOKEN } from "../constants/keys";
 import axiosInstance from "../api/axios-instance";
 import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
 const url = "/auth/verify-token";
 export default function useAuth() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
   const mutation = useMutation({
-    mutationFn: () => {
-      return axiosInstance.post(url, {
-        token: localStorage.getItem(USER_TOKEN),
-      });
-    },
+    mutationFn: async () => {
+      const token = localStorage.getItem(USER_TOKEN)
+      if(!token) return Promise.reject("No token found")
+
+      
+try {
+  const res = await axiosInstance.post(url, {
+    token,
+  });
+  return res;
+} catch (error) {
+  if (axios.isAxiosError(error)) {
+     if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    throw error.response.data
+  } else if (error.request) {
+    // The request was made but no response was received
+    console.log(error.request);
+    throw error
+
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log('Error', error.message);
+    throw error
+
+  }}
+ 
+}
+}
+      
+
   });
 
   useEffect(() => {
@@ -18,8 +46,10 @@ export default function useAuth() {
   }, []);
 
   useEffect(() => {
+    
     if (mutation.isError) {
       setIsLoggedIn(false);
+     localStorage.removeItem(USER_TOKEN)
     } else if (mutation.isSuccess) {
       setIsLoggedIn(true);
     }
