3d98fe224b94581e2c9ce8635bb7173bb0b05de3
khaled-sobhy <khaledloda696@gmail.com>
2023-05-27 14:30:59 +0300

    Adding logout function

--- frontend/src/components/DropDown.tsx
+++ frontend/src/components/DropDown.tsx
@@ -1,62 +1,97 @@
-import downIcon from "../assets/icons/caret-down-outline.svg"
-import personIcon from "../assets/icons/person-circle.svg"
-import groupIcon from "../assets/icons/people-circle.svg"
+import downIcon from "../assets/icons/caret-down-outline.svg";
+import personIcon from "../assets/icons/person-circle.svg";
+import groupIcon from "../assets/icons/people-circle.svg";
 import logoutIcon from "../assets/icons/log-out-outline.svg";
-import { Menu } from '@headlessui/react';
-import { Link } from "react-router-dom";
+import { Menu } from "@headlessui/react";
+import { Link, useNavigate } from "react-router-dom";
+import { USER_TOKEN } from "../constants/keys";
+import { toast } from "react-hot-toast";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 interface IDropDownProps {
-    userName: string
+  userName: string;
 }
 
-
 const DropDown: React.FunctionComponent<IDropDownProps> = (props) => {
-    return <Menu>
-        {({ open }) =>
-            <div className='relative  '>
-                <Menu.Button className="flex items-center gap-2 text-sm hover:opacity-80">{props.userName}<img src={downIcon} className={`w-3 h-3  ${open && "rotate-180"} transition-transform`} /></Menu.Button>
-                <Menu.Items className={`flex flex-col gap-2  p-3 bg-white border rounded-xl w-48 overflow-hidden absolute right-0 top-[200%] shadow`}>
-                    <Menu.Item>
-                        {({ active }) => (
-                            <Link
-                                className={`${active && 'bg-[#F2F2F2]'} rounded-lg px-3 py-2 flex gap-2 text-[#4F4F4F]`}
-                                to={"/"}
-                            >
-                                <img src={personIcon} className="w-6" />
-                                My Profile
-                            </Link>
-                        )}
-                    </Menu.Item>
-                    <Menu.Item>
-                        {({ active }) => (
-                            <a
-                                className={`${active && 'bg-[#F2F2F2]'} rounded-lg px-3 py-2 flex gap-2 text-[#4F4F4F]`}
-                                href="#"
-                            >
-                                <img src={groupIcon} className="w-6" />
-                                Group Chat
-                            </a>
-                        )}
-                    </Menu.Item>
-                    <div className="border-t"></div>
-                    <Menu.Item>
-                        {({ active }) => (
-                            <a
-                                className={`${active && 'bg-[#F2F2F2]'} rounded-lg px-3 py-2 flex gap-2 text-[#EB5757]`}
-                                href="#"
-                            >
-                                <img src={logoutIcon} className="w-6" />
-                                Logout
-                            </a>
-                        )}
-                    </Menu.Item>
-                </Menu.Items>
+  const { reCheck } = useContext(AuthContext);
 
-            </div>
-        }
+  const loggingOut = () => {
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        localStorage.removeItem(USER_TOKEN);
+        reCheck();
+        resolve("Logged out");
+      }, 800);
+    });
+
+    toast.promise(promise, {
+      loading: "Logging out ...",
+      success: "Logged out",
+      error: "Error when Logging out",
+    });
+  };
+  return (
+    <Menu>
+      {({ open }) => (
+        <div className="relative  ">
+          <Menu.Button className="flex items-center gap-2 text-sm hover:opacity-80">
+            {props.userName}
+            <img
+              src={downIcon}
+              className={`h-3 w-3  ${
+                open && "rotate-180"
+              } transition-transform`}
+            />
+          </Menu.Button>
+          <Menu.Items
+            className={`absolute right-0 top-[200%]  flex w-48 flex-col gap-2 overflow-hidden rounded-xl border bg-white p-3 shadow`}
+          >
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  className={`${
+                    active && "bg-[#F2F2F2]"
+                  } flex gap-2 rounded-lg px-3 py-2 text-[#4F4F4F]`}
+                  to={"/"}
+                >
+                  <img src={personIcon} className="w-6" />
+                  My Profile
+                </Link>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`${
+                    active && "bg-[#F2F2F2]"
+                  } flex gap-2 rounded-lg px-3 py-2 text-[#4F4F4F]`}
+                  href="#"
+                >
+                  <img src={groupIcon} className="w-6" />
+                  Group Chat
+                </a>
+              )}
+            </Menu.Item>
+            <div className="border-t"></div>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  className={`${
+                    active && "bg-[#F2F2F2]"
+                  } flex gap-2 rounded-lg px-3 py-2 text-[#EB5757]`}
+                  onClick={loggingOut}
+                >
+                  <img src={logoutIcon} className="w-6" />
+                  Logout
+                </button>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </div>
+      )}
     </Menu>
-
+  );
 };
-
 
 export default DropDown;
