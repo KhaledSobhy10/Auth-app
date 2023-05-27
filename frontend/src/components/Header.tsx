c2c31b1ebc51e08c18bbe20e7ed97a2887d2778e
khaled-sobhy <khaledloda696@gmail.com>
2023-05-27 14:31:20 +0300

    Adding user photo  (Avatar)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,6 +5,7 @@ import logo from "../assets/icons/logo.svg";
 import DropDown from "./DropDown";
 interface IHeaderProps {
   userName: string;
+  avatar?: string;
 }
 
 const Header: React.FunctionComponent<IHeaderProps> = (props) => {
@@ -12,7 +13,7 @@ const Header: React.FunctionComponent<IHeaderProps> = (props) => {
     <header className="container flex justify-between px-6 py-6 ">
       <img src={logo} />
       <div className="flex items-center gap-3">
-        <img src={placeholder} className="w-5 rounded-lg" />
+        <img src={props.avatar || placeholder} className="w-5 rounded-lg" />
         <DropDown userName={props.userName} />
       </div>
     </header>
