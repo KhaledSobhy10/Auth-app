8131d300533e061e514113d228713364fe04c8a6
KhaledSobhy22 <Khaled.sobhy@easwaaqmisr.com>
2023-05-30 16:26:24 +0300

    Make password not required in db

--- backend/src/middleware/isValidProfile.ts
+++ backend/src/middleware/isValidProfile.ts
@@ -1,20 +1,39 @@
 import { NextFunction, Request, Response } from "express";
-import { ProfileCreateInputSchema } from "../../prisma/generated/zod";
 import { getKeysWithError } from "../util/zodErrorFormater";
+import { z } from "zod";
 
-export const isValidProfileInputs = (req: Request, res: Response, next: NextFunction)=>{
-    const data = { ...req.body };
-    const validate = ProfileCreateInputSchema.safeParse(data);
-    
-    if (!validate.success)
-       res.status(400).json({
-        success: false,
-        errors: getKeysWithError(validate.error.issues),
-        message: "Incorrect form data !",
-      });
-      else {
-        req.body = validate.data;
-        next();
-    }
+const schema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  name: z.string().length(100).optional().nullable(),
+  password: z
+    .string()
+    .min(8, { message: "password must be more than 8 characters" }),
+  photo_url: z.string().optional().nullable(),
+  bio: z.string().length(200).optional().nullable(),
+  phone: z
+    .string()
+    .min(8, { message: "phone must be 11 numbers" })
+    .optional()
+    .nullable(),
+});
 
-}
+export const isValidProfileInputs = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const data = { ...req.body };
+
+  const validate = schema.safeParse(data);
+
+  if (!validate.success)
+    res.status(400).json({
+      success: false,
+      errors: getKeysWithError(validate.error.issues),
+      message: "Incorrect form data !",
+    });
+  else {
+    req.body = validate.data;
+    next();
+  }
+};
