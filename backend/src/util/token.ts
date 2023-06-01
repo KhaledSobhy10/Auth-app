808c711d5a35a1c239622ce18c2fe4be7481fa36
khaled-sobhy <khaledloda696@gmail.com>
2023-06-01 21:33:17 +0300

    Use token from env

--- backend/src/util/token.ts
+++ backend/src/util/token.ts
@@ -4,7 +4,7 @@ export const generateToken = (data: any, expiresIn: string | number = "1h") => {
   return new Promise((resolve, reject) => {
     jwt.sign(
       data,
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJmb28iOiJiYXNyIiwiaWF0IjoxNjg0MjMzMASDASDjQ0fQpSmfmR34bv_VpVcBdvynfTfwnlnpN_z84Y8sNoVe7fM",
+      process.env.TOKEN_SECRET as string,
       { expiresIn },
       (err, token) => {
         if (err) {
@@ -18,15 +18,11 @@ export const generateToken = (data: any, expiresIn: string | number = "1h") => {
 
 export const verifyToken = (token: string) => {
   return new Promise((resolve, reject) => {
-    jwt.verify(
-      token,
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9eyJmb28iOiJiYXNyIiwiaWF0IjoxNjg0MjMzMASDASDjQ0fQpSmfmR34bv_VpVcBdvynfTfwnlnpN_z84Y8sNoVe7fM",
-      (err, decoded) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(decoded);
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (err, decoded) => {
+      if (err) {
+        reject(err);
       }
-    );
+      resolve(decoded);
+    });
   });
 };
