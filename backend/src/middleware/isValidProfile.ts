ead96b5ac7780fb94a42fba9649e48addc341a77
khaled-sobhy <khaledloda696@gmail.com>
2023-06-02 10:18:00 +0300

    Fix name and bio length validation

--- backend/src/middleware/isValidProfile.ts
+++ backend/src/middleware/isValidProfile.ts
@@ -4,12 +4,12 @@ import { z } from "zod";
 
 const schema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
-  name: z.string().length(100).optional().nullable(),
+  name: z.string().max(30).optional().nullable(),
   password: z
     .string()
     .min(8, { message: "password must be more than 8 characters" }),
   photo_url: z.string().optional().nullable(),
-  bio: z.string().length(200).optional().nullable(),
+  bio: z.string().max(200).optional().nullable(),
   phone: z
     .string()
     .min(8, { message: "phone must be 11 numbers" })
