2ebc20ea2eb71d6cc6f146687ea174b8d1730779
khaled-sobhy <khaledloda696@gmail.com>
2023-06-03 23:58:42 +0300

    Add google and github auth

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,6 +1,14 @@
 import { Response, Request, NextFunction } from "express";
 import prismaDB from "../util/db";
 import { generateToken, verifyToken } from "../util/token";
+import {
+  getAccessTokenFromCode,
+  getGoogleUserInfo,
+} from "../util/googleHelper";
+import {
+  getGithubAccessTokenFromCode,
+  getGithubUserInfo,
+} from "../util/githubHelper";
 
 export const postLogin = async (req: Request, res: Response) => {
   // Check if data is exist in DB
@@ -41,4 +49,112 @@ export const postVerifyToken = async (req: Request, res: Response) => {
     .catch((error) => {
       res.status(401).json({ success: false, message: error.message });
     });
+};
+
+/**
+ * Handles the POST request for Google OAuth login.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @returns A redirect response to the login page with a JWT token.
+ */
+export const postWithGoogle = async (req: Request, res: Response) => {
+  try {
+    // Get the access token from the authorization code sent by Google.
+    const token = await getAccessTokenFromCode(req.query.code as string);
+
+    // Use the access token to get the user information from Google.
+    const googleUserData = await getGoogleUserInfo(token);
+
+    // Check if the user information includes an email address.
+    const email = googleUserData.email;
+    if (!email) {
+      console.log(
+        "🚀 ~ file: auth.ts:56 ~ postWithGoogle ~ unexpected response from google"
+      );
+      throw Error("unexpected response from google");
+    }
+
+    // Find or create a user profile in the database based on the email address.
+    let profile = await prismaDB.profile.findUnique({ where: { email } });
+    if (!profile) {
+      profile = await prismaDB.profile.create({
+        data: {
+          email: googleUserData.email,
+          photo_url: googleUserData.picture,
+          name: googleUserData.name,
+        },
+      });
+    }
+
+    // Generate a JWT token for the user's profile ID.
+    const loginToken = await generateToken({ id: profile.id });
+
+    // Redirect to the login page with the JWT token in the URL.
+    res.cookie("token", loginToken, {
+      maxAge: 900000,
+      httpOnly: false,
+      domain: process.env.FRONT_END_URL,
+    });
+    res.setHeader("Set-Cookie", `token= ${loginToken} Secure Max-age=10 `);
+
+    return res.redirect(
+      `${process.env.FRONT_END_URL}/login?token=${loginToken}`
+    );
+  } catch (error) {
+    // Log the error and redirect to the login page without a token.
+    console.log("🚀 ~ file: auth.ts:56 ~ postWithGoogle", error);
+    return res.redirect(`${process.env.FRONT_END_URL}/login`);
+  }
+};
+
+/**
+ * Handles the POST request for Github OAuth login.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @returns A redirect response to the login page with a JWT token.
+ */
+export const postWithGithub = async (req: Request, res: Response) => {
+  try {
+    // Get the access token from the authorization code sent by github.
+    const token = await getGithubAccessTokenFromCode(req.query.code as string);
+
+    // Use the access token to get the user information from github.
+    const githubUserData = await getGithubUserInfo(token);
+
+    // Check if the user information includes an email address.
+    const email = githubUserData.email;
+    if (!email) {
+      console.log(
+        "🚀 ~ file: auth.ts:56 ~ postWithGithub ~ unexpected response from github"
+      );
+      throw Error("unexpected response from github");
+    }
+
+    // Find or create a user profile in the database based on the email address.
+    let profile = await prismaDB.profile.findUnique({ where: { email } });
+    if (!profile) {
+      profile = await prismaDB.profile.create({
+        data: {
+          ...githubUserData,
+        },
+      });
+    }
+
+    // Generate a JWT token for the user's profile ID.
+    const loginToken = await generateToken({ id: profile.id });
+    console.log(
+      "🚀 ~ file: auth.ts:148 ~ postWithGithub ~ loginToken:",
+      loginToken
+    );
+
+    // Redirect to the login page with the JWT token in the URL.
+
+    return res.redirect(
+      `${process.env.FRONT_END_URL}/login?token=${loginToken}`
+    );
+  } catch (error) {
+    // Log the error and redirect to the login page without a token.
+    console.log("🚀 ~ file: auth.ts:156 ~ postWithGithub", error);
+    return res.redirect(`${process.env.FRONT_END_URL}/login`);
+  }
 };
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,70 +1,44 @@
 import { Router } from "express";
-import { postLogin, postVerifyToken } from "../controllers/auth";
+import {
+  postWithGoogle,
+  postLogin,
+  postVerifyToken,
+  postWithGithub,
+} from "../controllers/auth";
 
-import axios from "axios";
 import { isValidateLoginInput } from "../middleware/isValidLoginInput";
-
+import { getGoogleLoginUrl } from "../util/googleHelper";
+import { getGithubLoginUrl } from "../util/githubHelper";
 const router = Router();
 
+// Authentication routes
 router.post("/login", [isValidateLoginInput], postLogin);
-
 router.post("/verify-token", postVerifyToken);
 
-router.get("/google/callback", (req, res) => {
-  getAccessTokenFromCode(req.query.code as string)
-    .then((data) => {
-      console.log(
-        "🚀 ~ file: auth.ts:21 ~ getAccessTokenFromCode ~ data:",
-        data
-      );
-      return getGoogleUserInfo(data);
-      // res.json({ success: true, data });
-    })
-    .then((data) => {
-      // should create user if not exist or just check if email is
-      res.json({ success: true, data });
-    })
-    .catch((err) => {
-      console.log("🚀 ~ file: auth.ts:32 ~ router.get ~ err:", err);
-      res.json({ success: false, err });
-    });
+// Google OAuth2 login flow
+router.get("/google-login-url", async (req, res) => {
+  try {
+    const url = getGoogleLoginUrl();
+    res.status(200).json({ success: true, data: { url } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 });
 
-router.get("/google/callback/token", (req, res) => {
-  console.log(
-    "🚀 ~ file: auth.ts:17 ~ router.get ~ req.body.token:",
-    req.body.token
-  );
-  // res.json({ success: true });
+router.get("/google/callback", postWithGoogle);
+
+// GitHub OAuth2 login flow
+router.get("/github-login-url", async (req, res) => {
+  try {
+    const url = getGithubLoginUrl();
+    res.status(200).json({ success: true, data: { url } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 });
 
-async function getGoogleUserInfo(access_token: string) {
-  const { data } = await axios({
-    url: "https://www.googleapis.com/oauth2/v2/userinfo",
-    method: "get",
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  });
-  console.log(data); // { id, email, given_name, family_name }
-  return data;
-}
-
-async function getAccessTokenFromCode(code: string | undefined) {
-  if (!code) return;
-  const { data } = await axios({
-    url: `https://oauth2.googleapis.com/token`,
-    method: "post",
-    data: {
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: process.env.APP_URL + "/api/auth/google/callback",
-      grant_type: "authorization_code",
-      code,
-    },
-  });
-  console.log(data); // { access_token, expires_in, token_type, refresh_token }
-  return data.access_token;
-}
+router.get("/github/callback", postWithGithub);
 
 export default router;
